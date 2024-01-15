00dd59a51c2e1706d3a9c84798a6db2257b56f0e
Rohan Kothakapu <rkothaka@gmail.com>
2024-01-15 16:46:28 -0500

    python connector validation

---
+++ DE/Spark, Hadoop, and Snowflake for Data Engineering/Week 2/validate_snowflake_conn.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import snowflake.connector
+import os
+from dotenv import load_dotenv
+
+
+# Load environment variables from .env
+load_dotenv()
+
+
+# Gets the version
+ctx = snowflake.connector.connect(
+    user=os.getenv("SNOWFLAKE_USER"),
+    password=os.getenv("SNOWFLAKE_PASSWORD"),
+    account=os.getenv("SNOWFLAKE_ACCOUNT")
+    )
+cs = ctx.cursor()
+try:
+    cs.execute("SELECT current_version()")
+    one_row = cs.fetchone()
+    print(one_row[0])
+finally:
+    cs.close()
+ctx.close()
